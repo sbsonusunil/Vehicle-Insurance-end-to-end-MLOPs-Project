020ea4ec1393cf5665da64a4637491eddbb4eb45
sbsonusunil <sbsonusunil@gmail.com>
2026-04-20 18:10:09 +0530

    logging & exception

---
+++ demo.py
@@ -0,0 +1,7 @@
+from src.logger import logging
+
+logging.debug("this is a debug message")
+logging.info("this is a info  message")
+logging.warning("this is a warning message")
+logging.error("this is an error message")
+logging.critical("this is a critical message")
